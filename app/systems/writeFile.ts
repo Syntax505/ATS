c9fa720b40a7ba946de3ee4f4130801011de60d5
Tony Siu <syntax@syntax404.site>
2025-03-28 10:00:28 +0000

    not done yet

--- app/systems/writeFile.ts
+++ app/systems/writeFile.ts
@@ -1,13 +1,24 @@
+ import jsPDF from "jspdf";
+ import autoTable from "jspdf-autotable";
+ 
  export async function writeData(data: any, dataToWrite: any) {
   try {
     console.log("Writing..");
+    let bodyArray: string[][] = [];
     dataToWrite.forEach((d) => {
       data.events[d.index].date = d.time.date;
       data.events[d.index].hour = d.time.hour;
       data.events[d.index].minute = d.time.minute;
     });
+    data.events.forEach((e) => {
+      bodyArray.push([e.title, e.date, e.hour + ":" + e.minute]);
+    });
+    const date = new Date()
+    const doc = new jsPDF();
+    doc.text(`Timetable - Generated on ${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate}`,10,10,{align: "center", horizontalScale: 5});
+    autoTable(doc, {startY: 20, head: [["Event Name", "Event Date", "Event Time"]], body: [bodyArray]});
     const jsonString = JSON.stringify(data, null, 2);
-    const blob = new Blob([jsonString], { type: "application/json" });
+    const blob = doc.output("blob", )
     const url = URL.createObjectURL(blob);
     return url;
   } catch (err) {
